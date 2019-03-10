fa023a1080aebe81f52496d33b621805986d6e2f
Bahis27 <46839998+Bahis27@users.noreply.github.com>
2019-03-10 20:57:26 +0300

    HW05: 1.1 is done.

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,22 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    @Transactional
+    Integer deleteByIdAndUserId(Integer id, Integer userId);
+
+    Optional<Meal> findByIdAndUserId (Integer id, Integer userId);
+
+    List<Meal> findAllByUserIdOrderByDateTimeDesc(Integer userId);
+
+    List<Meal> findAllByUserIdAndDateTimeBetweenOrderByDateTimeDesc(Integer userId, LocalDateTime startDate, LocalDateTime endDate);
 }
