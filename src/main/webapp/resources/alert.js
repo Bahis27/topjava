3ed2b29413dca9e9b0a6527175586ab3f76e3e95
Bahis27 <46839998+Bahis27@users.noreply.github.com>
2019-04-04 19:50:21 +0300

    HW07: js example

---
+++ src/main/webapp/resources/alert.js
@@ -0,0 +1,11 @@
+"use strict";
+//var name ="asd";
+//alert(typeof name);
+//[1, 2].forEach(alert);
+
+var result;
+result = prompt("Ответь! Прикольно?", "");
+alert("Ты ввел: " + result );
+
+var isOk = confirm("Точно?");
+isOk ? alert("!") : alert("?");
